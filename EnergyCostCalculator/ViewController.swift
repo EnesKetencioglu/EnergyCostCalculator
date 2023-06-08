f2768a2a27724244876cbc7e2663caca5cadd1b1
Enes Ketencioglu <enes.ketencioglu@gmail.com>
2023-06-08 10:54:01 +0300

    Initial Commit

---
+++ EnergyCostCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EnergyCostCalculator
+//
+//  Created by Enes Ketencioglu on 8.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EnergyCostCalculatorUITests/EnergyCostCalculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EnergyCostCalculatorUITestsLaunchTests.swift
+//  EnergyCostCalculatorUITests
+//
+//  Created by Enes Ketencioglu on 8.06.2023.
+//
+
+import XCTest
+
+final class EnergyCostCalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
